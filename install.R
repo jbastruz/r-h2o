8ee5c7b237f167b116f66efbfc33b31569d2690c
vinnys <vinnys@centos2.saulys.com>
2016-03-05 17:16:34 -0500

    small updates

--- install.R
+++ install.R
@@ -1,26 +1,18 @@
-
-# version to download
-h2oVersion <- "http://h2o-release.s3.amazonaws.com/h2o/rel-tibshirani/3/R"
-
 # The following two commands remove any previously installed H2O packages for R.
 if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
 if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
 
 # Next, we download packages that H2O depends on.
-if (! ("methods" %in% rownames(installed.packages()))) { install.packages("methods") }
-if (! ("statmod" %in% rownames(installed.packages()))) { install.packages("statmod") }
-if (! ("stats" %in% rownames(installed.packages()))) { install.packages("stats") }
-if (! ("graphics" %in% rownames(installed.packages()))) { install.packages("graphics") }
-if (! ("RCurl" %in% rownames(installed.packages()))) { install.packages("RCurl") }
-if (! ("rjson" %in% rownames(installed.packages()))) { install.packages("rjson") }
-if (! ("tools" %in% rownames(installed.packages()))) { install.packages("tools") }
-if (! ("utils" %in% rownames(installed.packages()))) { install.packages("utils") }
+pkgs <- c("methods","statmod","stats","graphics","RCurl","jsonlite","tools","utils")
+for (pkg in pkgs) {
+  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+}
 
 # Now we download, install and initialize the H2O package for R.
-install.packages("h2o", type="source", repos=h2oVersion)
+install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-turan/2/R")))
 
 library(h2o)
-localH2O = h2o.init()
+h2o.init(ip="localhost", startH2O = TRUE)
 
 # Finally, let's run a demo to see H2O at work.
 demo(h2o.kmeans)
