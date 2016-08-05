4fc25d65587e14df6dd59a59131014051b08e318
Vincent Saulys <vincent.saulys@gmail.com>
2016-08-05 19:29:21 -0400

    version update

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # version to download
-h2oVersion <- "http://h2o-release.s3.amazonaws.com/h2o/rel-turin/4/R"
+h2oVersion <- "http://h2o-release.s3.amazonaws.com/h2o/rel-turing/3/R"
 
 # The following two commands remove any previously installed H2O packages for R.
 if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
